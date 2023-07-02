9f183323b0e4f594da09bde455a90c7c589cb378
Dev Patel <109199551+ggpop00@users.noreply.github.com>
2023-07-02 19:29:34 +0530

    Add files via upload

---
+++ Assi-7.py
@@ -0,0 +1,27 @@
+def update_file(file="New.txt"):
+    try:
+        # Open the file in append mode
+        f = open(file, "a")
+        
+        # Write data to the file
+        roll_number = "107"
+        name = "Dev Patel"
+        class_name = "SYCO"
+        f.writelines([roll_number + "\n", name + "\n", class_name + "\n"])
+                
+        # Reopen the file in read mode
+        f = open(file, "r")
+        
+        # Read and print the data from the file
+        print(f.readlines())
+
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+    except IOError as e:
+        print(f"Error: {e}")
+
+# Call the function
+# update_file()
+update_file()
+
+
